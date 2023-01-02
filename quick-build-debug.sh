1e7a942b5ca2ccf55fec445e098beb2701b49299
Maynafiesta <tarikkartal2@hotmail.com>
2023-01-02 08:19:34 +0300

    shellcheck duzenlemeleri eklendi.

--- quick-build-debug.sh
+++ quick-build-debug.sh
@@ -2,8 +2,8 @@
 
 rm -rf cmake-build-debug-my
 mkdir cmake-build-debug-my
-cd cmake-build-debug-my
+cd cmake-build-debug-my || exit
 cmake .. -DCMAKE_BUILD_TYPE=Debug . 
-make
-cd .. 
+make -j "$(nproc)"
+cd .. || exit
 ./cmake-build-debug-my/smirVideoTool
--- quick-build-release.sh
+++ quick-build-release.sh
@@ -2,8 +2,8 @@
 
 rm -rf cmake-build-release-my
 mkdir cmake-build-release-my
-cd cmake-build-release-my
+cd cmake-build-release-my || exit
 cmake .. -DCMAKE_BUILD_TYPE=Release . 
-make
-cd .. 
+make -j "$(nproc)"
+cd ..  || exit
 ./cmake-build-release-my/smirVideoTool
